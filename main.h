2802cdf736a97656d798c7e8870a8b945f2f03b8
Soufiane <dahhani.yane@gmail.com>
2023-10-17 09:47:25 -0700

    task

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
+#ifndef MAIN_H
+#define MAINN_H
+
+
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
 int _printf(const char *format, ...);
+void print_buffer(char buffer[], int *buff_ind);
+
+#endif
 
