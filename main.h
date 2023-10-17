b2bd4d44751be39b9e75ff215c830a16824db420
Soufiane <dahhani.yane@gmail.com>
2023-10-17 06:48:03 -0700

    first task printf

---
+++ main.h
@@ -0,0 +1,1 @@
+int prinft(const format[],...);
---
+++ printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    int i = 0;
+    int count = 0;
+    
+    while (format[i] != '\0')
+    {
+        if (format[i] != '%')
+        {
+            write(STDOUT_FILENO, &format[i], 1);
+            count++;
+        }
+        else
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                {
+                    char ch = (char)va_arg(args, int);
+                    write(STDOUT_FILENO, &ch, 1);
+                    break;
+                }
+                case 's' :
+                {        const char* s = va_arg(args, const char*);
+             int len = 0;
+                while (s[len] != '\0')
+                  ++len;
+                    
+                write(STDOUT_FILENO, s, len); 
+                    break;}
+                default:
+                    break;
+            }
+        }
+        
+        i++;
+    }
+    
+    va_end(args);
+    
+    return count;
+}
+
