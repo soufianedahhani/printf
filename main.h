6ff527b69035db06479158a076a57c0e90489fd7
Soufiane <dahhani.yane@gmail.com>
2023-10-17 10:50:48 -0700

    lets try this

--- main.h
+++ main.h
@@ -1,18 +1,6 @@
 #ifndef MAIN_H
-#define MAINN_H
+#define MAIN_H
 
+int _printf(const char *format, ...);
 
-
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-
-int printf_string(va_list args) ;
-int printf_char(va_list args) ;
-int printf_37(va_list args) ;
-int printf_int(va_list args);
-
-#endif
-
+#endif /* MAIN_H */
--- printf.c
+++ printf.c
@@ -1,73 +1,57 @@
-#include "main.h"
-typedef struct {
-    const char* id;
-    int (*f)(va_list);
-} convert_match;
+#include <unistd.h>
+#include <stdarg.h>
 
-int printf_string(va_list args) {
-    const char* s = va_arg(args, const char*);
-    int len = 0;
-    while (s[len] != '\0')
-        ++len;
-    write(STDOUT_FILENO, s, len);
-    return len;
-}
-
-int printf_char(va_list args) {
-    int c = va_arg(args, int);
-    write(STDOUT_FILENO, &c, 1);
-    return 1;
-}
-
-int printf_37(va_list args) {
-    write(STDOUT_FILENO, "%", 1);
-    return 1;
-}
-
-int printf_int(va_list args) {
-    int num = va_arg(args, int);
-    char buffer[20];
-    int len = sprintf(buffer, "%d", num);
-    write(STDOUT_FILENO, buffer, len);
-    return len;
-}
-
-// Add implementations for other conversion specifiers as needed
-
-int my_printf(const char* format, ...)
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
 {
-    convert_match m[] = {
-        {"%s", printf_string},
-        {"%c", printf_char},
-        {"%%", printf_37},
-        {"%d", printf_int},
-        // Add other conversion specifiers and their corresponding functions here
-    };
-
     va_list args;
-    int i = 0, j, len = 0;
+    int count = 0;
+    char c;
+    char *str;
 
     va_start(args, format);
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return -1;
 
-    while (format[i] != '\0') {
-        j = 0;
-        while (j < sizeof(m) / sizeof(m[0])) {
-            if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1]) {
-                len += m[j].f(args);
-                i = i + 2;
-                break;
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            count++;
+        }
+        else
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    write(1, &c, 1);
+                    count++;
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    while (*str)
+                    {
+                        write(1, str, 1);
+                        str++;
+                        count++;
+                    }
+                    break;
+                default:
+                    write(1, format - 1, 2);
+                    count += 2;
+                    break;
             }
-            j++;
         }
-        if (j == sizeof(m) / sizeof(m[0])) {
-            write(STDOUT_FILENO, &format[i], 1);
-            len++;
-            i++;
-        }
+        format++;
     }
-    va_end(args);
-    return len;
-}
 
+    va_end(args);
+
+    return count;
+}
